39b2690a77d01f16e9ea7adf194512e3c7b9e438
Sainath Reddy <37265003+sainath-reddiee@users.noreply.github.com>
2025-10-13 23:13:23 +0530

    Updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,237 +1,99 @@
-Run SamKirkland/FTP-Deploy-Action@v4.3.5
-----------------------------------------------------------------
-🚀 Thanks for using ftp-deploy. Let's deploy some stuff!   
-----------------------------------------------------------------
-If you found this project helpful, please support it
-by giving it a ⭐ on Github --> https://github.com/SamKirkland/FTP-Deploy-Action
-or add a badge 🏷️ to your projects readme --> https://github.com/SamKirkland/FTP-Deploy-Action#badge
-----------------------------------------------------------------
-Last published on 📅 Monday, October 13, 2025 at 5:24 PM
-----------------------------------------------------------------
-Local Files:	52
-Server Files:	53
-----------------------------------------------------------------
-Calculating differences between client & server
-----------------------------------------------------------------
-📄 Upload: assets/index-BCUn2FOV.css
-📄 Upload: assets/js/AboutPage-BfYivOTa.js
-📄 Upload: assets/js/AdPlacement-DT0UOMgb.js
-📄 Upload: assets/js/alert-circle-DBG3A_Eu.js
-📄 Upload: assets/js/AllArticlesPage-CMcr6K4d.js
-📄 Upload: assets/js/ApiDebugger-BCsVCCJ6.js
-📄 Upload: assets/js/App-bzaP8BA8.js
-📄 Upload: assets/js/arrow-left-D9w52iey.js
-📄 Upload: assets/js/arrow-right-sBMRVqBD.js
-📄 Upload: assets/js/ArticlePage-Be35at4A.js
-📄 Upload: assets/js/button-U0XuEVDK.js
-📄 Upload: assets/js/CategoryPage-B2J11wq5.js
-📄 Upload: assets/js/check-circle-ZgZS4JSr.js
-📄 Upload: assets/js/ContactPage-BGyT9SWI.js
-📄 Upload: assets/js/DisclaimerPage-DwIxQ0br.js
-📄 Upload: assets/js/FeaturedPosts-DLVF0VFc.js
-📄 Upload: assets/js/HomePage-BaOBPpxH.js
-📄 Upload: assets/js/index-DgGBFurn.js
-📄 Upload: assets/js/input-BXR-PCaa.js
-📄 Upload: assets/js/loader-mcILRshx.js
-📄 Upload: assets/js/Newsletter-DWP5Ayte.js
-📄 Upload: assets/js/NewsletterPage-iYhwyY4g.js
-📄 Upload: assets/js/PostCardSkeleton-CiFz7qCm.js
-📄 Upload: assets/js/PostListItemSkeleton-CMJFzX35.js
-📄 Upload: assets/js/PrivacyPolicyPage-BLattnqT.js
-📄 Upload: assets/js/RecentPosts-DNw-Vrbe.js
-📄 Upload: assets/js/search-_4qddumz.js
-📄 Upload: assets/js/send-DQdZ4tZh.js
-📄 Upload: assets/js/sparkles-CzQ2VbiY.js
-📄 Upload: assets/js/tag-BrRU9-m7.js
-📄 Upload: assets/js/TagPage-BZe4dQ-_.js
-📄 Upload: assets/js/TagsArchivePage-BZJ-XBVs.js
-📄 Upload: assets/js/TechCategories-CMk7DTyF.js
-📄 Upload: assets/js/TermsOfServicePage-Cajxa_eW.js
-📄 Upload: assets/js/TrendingPosts-C7o-ffyp.js
-📄 Upload: assets/js/users-C_ZNf2rY.js
-📄 Upload: assets/js/utils-BJ2ZPDDR.js
-🔁 File replace: index.html
-📄 Delete: assets/js/AboutPage-CImXSFFh.js    
-📄 Delete: assets/js/AdPlacement-dS_r1sfg.js    
-📄 Delete: assets/js/alert-circle-CadiLKYG.js    
-📄 Delete: assets/js/AllArticlesPage-_ExXgYCE.js    
-📄 Delete: assets/js/ApiDebugger-BgKMFK0e.js    
-📄 Delete: assets/js/App-D41NDLSS.js    
-📄 Delete: assets/js/arrow-left-COU8Buhb.js    
-📄 Delete: assets/js/arrow-right-CEbCzagy.js    
-📄 Delete: assets/js/ArticlePage-BrcVugCg.js    
-📄 Delete: assets/js/button-CASZYJJM.js    
-📄 Delete: assets/js/CategoryPage-BLYyfxyx.js    
-📄 Delete: assets/js/check-circle-DQOPOVSz.js    
-📄 Delete: assets/js/ContactPage-BGziBLBQ.js    
-📄 Delete: assets/js/DisclaimerPage-CBLWk_ob.js    
-📄 Delete: assets/js/FeaturedPosts-Z8ZKt3Lq.js    
-📄 Delete: assets/js/HomePage-gL0BdBqY.js    
-📄 Delete: assets/js/index-DMyW1Idv.js    
-📄 Delete: assets/js/input-BumsNalx.js    
-📄 Delete: assets/js/loader-e0N_KXDX.js    
-📄 Delete: assets/js/Newsletter-Dv_qL0FE.js    
-📄 Delete: assets/js/NewsletterPage-BcLHgBPz.js    
-📄 Delete: assets/js/PostCardSkeleton-CFD0jNfS.js    
-📄 Delete: assets/js/PostListItemSkeleton-VsRemjgH.js    
-📄 Delete: assets/js/PrivacyPolicyPage-BKs60crz.js    
-📄 Delete: assets/js/RecentPosts-_ykXpA36.js    
-📄 Delete: assets/js/search-Bof5A357.js    
-📄 Delete: assets/js/send-CGyWYjHI.js    
-📄 Delete: assets/js/sparkles-Dub-Uqin.js    
-📄 Delete: assets/js/tag-B3J4J2_B.js    
-📄 Delete: assets/js/TagPage-F1MPc70b.js    
-📄 Delete: assets/js/TagsArchivePage-eDSow_bj.js    
-📄 Delete: assets/js/TechCategories-Ci4AC0As.js    
-📄 Delete: assets/js/TermsOfServicePage-Becnwwwd.js    
-📄 Delete: assets/js/TrendingPosts-BKW345C9.js    
-📄 Delete: assets/js/users-D8tuH3By.js    
-📄 Delete: assets/js/utils-Bcdejx4T.js    
-📁 Delete: assets/css    
-⚖️  File content is the same, doing nothing: .htaccess
-⚖️  File content is the same, doing nothing: 964879007f504f7656ed5318909f7c79.txt
-⚖️  File content is the same, doing nothing: assets/js/framer-motion-BRSuW487.js
-⚖️  File content is the same, doing nothing: assets/js/react-vendor-GBB5G_d3.js
-⚖️  File content is the same, doing nothing: assets/js/ui-vendor-JNZcohrf.js
-⚖️  File content is the same, doing nothing: assets/js/useIntersectionObserver-DlJteqUL.js
-⚖️  File content is the same, doing nothing: assets/js/wordpress-api-9vctYLE5.js
-⚖️  File content is the same, doing nothing: indexnow-key.txt
-⚖️  File content is the same, doing nothing: logo.png
-⚖️  File content is the same, doing nothing: og-image.jpg
-⚖️  File content is the same, doing nothing: robots.txt
-⚖️  File content is the same, doing nothing: sitemap.xml
-----------------------------------------------------------------
-Making changes to 75 files/folders to sync server state
-Uploading: 3*** kB -- Deleting: 3*** kB -- Replacing: 6.91 kB
-----------------------------------------------------------------
-uploading "assets/index-BCUn2FOV.css"
-uploading "assets/js/AboutPage-BfYivOTa.js"
-uploading "assets/js/AdPlacement-DT0UOMgb.js"
-uploading "assets/js/alert-circle-DBG3A_Eu.js"
-uploading "assets/js/AllArticlesPage-CMcr6K4d.js"
-uploading "assets/js/ApiDebugger-BCsVCCJ6.js"
-uploading "assets/js/App-bzaP8BA8.js"
-uploading "assets/js/arrow-left-D9w52iey.js"
-uploading "assets/js/arrow-right-sBMRVqBD.js"
-uploading "assets/js/ArticlePage-Be35at4A.js"
-uploading "assets/js/button-U0XuEVDK.js"
-uploading "assets/js/CategoryPage-B2J11wq5.js"
-uploading "assets/js/check-circle-ZgZS4JSr.js"
-uploading "assets/js/ContactPage-BGyT9SWI.js"
-uploading "assets/js/DisclaimerPage-DwIxQ0br.js"
-uploading "assets/js/FeaturedPosts-DLVF0VFc.js"
-uploading "assets/js/HomePage-BaOBPpxH.js"
-uploading "assets/js/index-DgGBFurn.js"
-uploading "assets/js/input-BXR-PCaa.js"
-uploading "assets/js/loader-mcILRshx.js"
-uploading "assets/js/Newsletter-DWP5Ayte.js"
-uploading "assets/js/NewsletterPage-iYhwyY4g.js"
-uploading "assets/js/PostCardSkeleton-CiFz7qCm.js"
-uploading "assets/js/PostListItemSkeleton-CMJFzX35.js"
-uploading "assets/js/PrivacyPolicyPage-BLattnqT.js"
-uploading "assets/js/RecentPosts-DNw-Vrbe.js"
-uploading "assets/js/search-_4qddumz.js"
-uploading "assets/js/send-DQdZ4tZh.js"
-uploading "assets/js/sparkles-CzQ2VbiY.js"
-uploading "assets/js/tag-BrRU9-m7.js"
-uploading "assets/js/TagPage-BZe4dQ-_.js"
-uploading "assets/js/TagsArchivePage-BZJ-XBVs.js"
-uploading "assets/js/TechCategories-CMk7DTyF.js"
-uploading "assets/js/TermsOfServicePage-Cajxa_eW.js"
-uploading "assets/js/TrendingPosts-C7o-ffyp.js"
-uploading "assets/js/users-C_ZNf2rY.js"
-uploading "assets/js/utils-BJ2ZPDDR.js"
-replacing "index.html"
-removing "assets/js/AboutPage-CImXSFFh.js"
-File not found or you don't have access to the file - skipping...
-removing "assets/js/AdPlacement-dS_r1sfg.js"
-File not found or you don't have access to the file - skipping...
-removing "assets/js/alert-circle-CadiLKYG.js"
-File not found or you don't have access to the file - skipping...
-removing "assets/js/AllArticlesPage-_ExXgYCE.js"
-File not found or you don't have access to the file - skipping...
-removing "assets/js/ApiDebugger-BgKMFK0e.js"
-File not found or you don't have access to the file - skipping...
-removing "assets/js/App-D41NDLSS.js"
-File not found or you don't have access to the file - skipping...
-removing "assets/js/arrow-left-COU8Buhb.js"
-File not found or you don't have access to the file - skipping...
-removing "assets/js/arrow-right-CEbCzagy.js"
-File not found or you don't have access to the file - skipping...
-removing "assets/js/ArticlePage-BrcVugCg.js"
-File not found or you don't have access to the file - skipping...
-removing "assets/js/button-CASZYJJM.js"
-File not found or you don't have access to the file - skipping...
-removing "assets/js/CategoryPage-BLYyfxyx.js"
-File not found or you don't have access to the file - skipping...
-removing "assets/js/check-circle-DQOPOVSz.js"
-File not found or you don't have access to the file - skipping...
-removing "assets/js/ContactPage-BGziBLBQ.js"
-File not found or you don't have access to the file - skipping...
-removing "assets/js/DisclaimerPage-CBLWk_ob.js"
-File not found or you don't have access to the file - skipping...
-removing "assets/js/FeaturedPosts-Z8ZKt3Lq.js"
-File not found or you don't have access to the file - skipping...
-removing "assets/js/HomePage-gL0BdBqY.js"
-File not found or you don't have access to the file - skipping...
-removing "assets/js/index-DMyW1Idv.js"
-File not found or you don't have access to the file - skipping...
-removing "assets/js/input-BumsNalx.js"
-File not found or you don't have access to the file - skipping...
-removing "assets/js/loader-e0N_KXDX.js"
-File not found or you don't have access to the file - skipping...
-removing "assets/js/Newsletter-Dv_qL0FE.js"
-File not found or you don't have access to the file - skipping...
-removing "assets/js/NewsletterPage-BcLHgBPz.js"
-File not found or you don't have access to the file - skipping...
-removing "assets/js/PostCardSkeleton-CFD0jNfS.js"
-File not found or you don't have access to the file - skipping...
-removing "assets/js/PostListItemSkeleton-VsRemjgH.js"
-File not found or you don't have access to the file - skipping...
-removing "assets/js/PrivacyPolicyPage-BKs60crz.js"
-File not found or you don't have access to the file - skipping...
-removing "assets/js/RecentPosts-_ykXpA36.js"
-File not found or you don't have access to the file - skipping...
-removing "assets/js/search-Bof5A357.js"
-File not found or you don't have access to the file - skipping...
-removing "assets/js/send-CGyWYjHI.js"
-File not found or you don't have access to the file - skipping...
-removing "assets/js/sparkles-Dub-Uqin.js"
-File not found or you don't have access to the file - skipping...
-removing "assets/js/tag-B3J4J2_B.js"
-File not found or you don't have access to the file - skipping...
-removing "assets/js/TagPage-F1MPc70b.js"
-File not found or you don't have access to the file - skipping...
-removing "assets/js/TagsArchivePage-eDSow_bj.js"
-File not found or you don't have access to the file - skipping...
-removing "assets/js/TechCategories-Ci4AC0As.js"
-File not found or you don't have access to the file - skipping...
-removing "assets/js/TermsOfServicePage-Becnwwwd.js"
-File not found or you don't have access to the file - skipping...
-removing "assets/js/TrendingPosts-BKW345C9.js"
-File not found or you don't have access to the file - skipping...
-removing "assets/js/users-D8tuH3By.js"
-File not found or you don't have access to the file - skipping...
-removing "assets/js/utils-Bcdejx4T.js"
-File not found or you don't have access to the file - skipping...
-removing folder "/***/assets/css"
+// src/main.jsx - OPTIMIZED FONT LOADING
+import React, { Suspense } from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+import '@/index.css';
+import { initThirdPartyScripts } from '@/utils/scriptLoader';
 
-----------------------------------------------------------------
---------------  🔥🔥🔥 an error occurred  🔥🔥🔥  --------------
-----------------------------------------------------------------
+const App = React.lazy(() => import('@/App'));
 
-----------------------------------------------------------------
-----------------------  full error below  ----------------------
-----------------------------------------------------------------
+const PageLoader = () => (
+  <div className="min-h-screen bg-gradient-to-br from-slate-900 via-blue-900 to-indigo-900 flex items-center justify-center">
+    <div className="flex flex-col items-center space-y-4">
+      <div className="w-8 h-8 border-4 border-blue-400 border-t-transparent rounded-full animate-spin"></div>
+      <p className="text-blue-300 font-medium">Loading DataEngineer Hub...</p>
+    </div>
+  </div>
+);
 
-FTPError: 550 /***/assets/css: No such file or directory
-    at FTPContext._onControlSocketData (/home/runner/work/_actions/SamKirkland/FTP-Deploy-Action/v4.3.5/dist/index.js:5263:39)
-    at Socket.<anonymous> (/home/runner/work/_actions/SamKirkland/FTP-Deploy-Action/v4.3.5/dist/index.js:5107:44)
-    at Socket.emit (node:events:524:28)
-    at addChunk (node:internal/streams/readable:561:12)
-    at readableAddChunkPushByteMode (node:internal/streams/readable:512:3)
-    at Readable.push (node:internal/streams/readable:392:5)
-    at TCP.onStreamRead (node:internal/stream_base_commons:191:23) {
-  code: 550
+// OPTIMIZED FONT LOADING
+const loadFonts = () => {
+  // Preconnect to font domains
+  const preconnectGoogle = document.createElement('link');
+  preconnectGoogle.rel = 'preconnect';
+  preconnectGoogle.href = 'https://fonts.googleapis.com';
+  document.head.appendChild(preconnectGoogle);
+
+  const preconnectGstatic = document.createElement('link');
+  preconnectGstatic.rel = 'preconnect';
+  preconnectGstatic.href = 'https://fonts.gstatic.com';
+  preconnectGstatic.crossOrigin = 'anonymous';
+  document.head.appendChild(preconnectGstatic);
+
+  // Preload critical font file
+  const preloadFont = document.createElement('link');
+  preloadFont.rel = 'preload';
+  preloadFont.as = 'font';
+  preloadFont.type = 'font/woff2';
+  preloadFont.crossOrigin = 'anonymous';
+  preloadFont.href = 'https://fonts.gstatic.com/s/inter/v12/UcCO3FwrK3iLTeHuS_fvQtMwCp50KnMw2boKoduKmMEVuLyfAZ9hiA.woff2';
+  document.head.appendChild(preloadFont);
+
+  // Load font stylesheet with font-display: swap
+  const fontLink = document.createElement('link');
+  fontLink.rel = 'stylesheet';
+  fontLink.href = 'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap';
+  fontLink.media = 'all';
+  document.head.appendChild(fontLink);
+};
+
+// Initialize fonts
+loadFonts();
+
+// Performance monitoring
+if (typeof window !== 'undefined' && 'performance' in window) {
+  window.addEventListener('load', () => {
+    const perfData = performance.getEntriesByType('navigation')[0];
+    console.log('🚀 Performance Metrics:', {
+      'Load Time': `${Math.round(perfData.loadEventEnd - perfData.fetchStart)}ms`,
+      'DOMContentLoaded': `${Math.round(perfData.domContentLoadedEventEnd - perfData.fetchStart)}ms`,
+      'First Paint': `${Math.round(performance.getEntriesByType('paint')[0]?.startTime || 0)}ms`
+    });
+  });
 }
-Error: FTPError: 550 /***/assets/css: No such file or directory
+
+// Log environment configuration in development
+if (import.meta.env.DEV) {
+  console.log('🔧 Development Environment Configuration:', {
+    MODE: import.meta.env.MODE,
+    DEV: import.meta.env.DEV,
+    PROD: import.meta.env.PROD,
+    VITE_ADS_ENABLED: import.meta.env.VITE_ADS_ENABLED,
+    VITE_ADSENSE_PUBLISHER_ID: import.meta.env.VITE_ADSENSE_PUBLISHER_ID ? 
+      import.meta.env.VITE_ADSENSE_PUBLISHER_ID.slice(0, 15) + '...' : 
+      '❌ Not Set',
+    VITE_GA_MEASUREMENT_ID: import.meta.env.VITE_GA_MEASUREMENT_ID || '❌ Not Set'
+  });
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <HelmetProvider>
+      <BrowserRouter>
+        <Suspense fallback={<PageLoader />}>
+          <App />
+        </Suspense>
+      </BrowserRouter>
+    </HelmetProvider>
+  </React.StrictMode>
+);
+
+// Initialize third-party scripts after app loads
+if (typeof window !== 'undefined') {
+  setTimeout(() => {
+    initThirdPartyScripts();
+  }, 1000);
+}
