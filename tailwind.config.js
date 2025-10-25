8d5e0f9c8517ca3c3a72e9647338662a326d5b39
Sainath Reddy <37265003+sainath-reddiee@users.noreply.github.com>
2025-10-25 10:10:33 +0530

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,8 @@ module.exports = {
 		'./pages/**/*.{js,jsx}',
 		'./components/**/*.{js,jsx}',
 		'./app/**/*.{js,jsx}',
-		'./src/**/*.{js,jsx}',
+		"./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
 		container: {
