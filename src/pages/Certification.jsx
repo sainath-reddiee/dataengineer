38b858e37b06e4ee0786a7c69c20e7fff8f96d60
Sainath Reddy <37265003+sainath-reddiee@users.noreply.github.com>
2025-12-30 09:23:16 +0530

    Updated Certification.jsx

---
+++ src/pages/Certification.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Certification = () => {
+  return (
+    <div className="w-full h-screen flex flex-col">
+      {/* Optional: Add a small header or breadcrumb here if you want */}
+      
+      <div className="flex-grow w-full h-full relative">
+        <iframe
+          src="https://snowcertify.com/?embed=true" 
+          title="Snowflake Certification Prep"
+          width="100%"
+          height="100%"
+          style={{ border: 'none', minHeight: '100vh' }}
+          allow="camera; microphone; clipboard-read; clipboard-write;"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Certification;
